319367b9a1c521b8bdf05df41dfa31a38c4b2371
Merlin Bögershausen <m.boegershausen@gmail.com>
2021-02-03 16:54:31 +0100

    fix arch type

--- build-release.sh
+++ build-release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-env GOOS=linux GOARCH=arm go build -o go-state-store_linux_arm
+env GOOS=linux GOARCH=arm go build -o go-state-store_linux_armhf
 env GOOS=linux GOARCH=amd64 go build -o go-state-store_linux_amd64
